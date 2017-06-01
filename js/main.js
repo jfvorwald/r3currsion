1dab362c0e6006074875d3a5685be09201159531
r3currsion <jfvorwald@gmail.com>
2017-06-01 00:40:57 -0400

    Have to rollback due to deploy service outage.

--- js/main.js
+++ js/main.js
@@ -18,16 +18,16 @@ $(function() {
         }
     });
 
-    // For the "Init()" blinking text.
-    // $("#cover-title").typed({
-    //     strings: ["Init ();"],
-    //     typeSpeed: 200,
-    //     cursorChar: "█",
-    //     startDelay: 150,
-    //     callback: function() {
-    //         // Can do callback function here after done typing.
-    //     },
-    // });
+    For the "Init()" blinking text.
+    $("#cover-title").typed({
+        strings: ["Init ();"],
+        typeSpeed: 200,
+        cursorChar: "█",
+        startDelay: 150,
+        callback: function() {
+            // Can do callback function here after done typing.
+        },
+    });
 
     // A volume controller for the legacy music player.
     $("#vol_control").slider({
